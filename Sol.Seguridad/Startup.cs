c7de8d9a78d8c4f1d7c6ebcf9f7a62d94c84dcdf
Miguel Salvador <msalvadorp@hotmail.com>
2020-11-14 17:31:35 -0500

    Ajustes docker file

--- Sol.Seguridad/Startup.cs
+++ Sol.Seguridad/Startup.cs
@@ -5,25 +5,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sol.Seguridad.Configs;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace Sol.Seguridad
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        private readonly IWebHostEnvironment webHostEnvironmentv;
+
+        public Startup(
+            IConfiguration configuration,
+            IWebHostEnvironment webHostEnvironmentv)
+        {
+            this.configuration = configuration;
+            this.webHostEnvironmentv = webHostEnvironmentv;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var cert = new X509Certificate2
+                (Path.Combine(webHostEnvironmentv.ContentRootPath, 
+                configuration["IdentityServer:FileName"]),
+                configuration["IdentityServer:Password"], 
+                X509KeyStorageFlags.MachineKeySet);
+
             services.AddIdentityServer()
                 .AddInMemoryIdentityResources(IdentityConfig.GetIdentityResources())
                 .AddInMemoryApiScopes(IdentityConfig.ApiScopes)
                 .AddInMemoryApiResources(IdentityConfig.ApiResources)
                 .AddInMemoryClients(IdentityConfig.GetClients())
                 .AddTestUsers(IdentityConfig.GestUsers())
-                .AddDeveloperSigningCredential()
+                .AddSigningCredential(cert)
+                //.AddDeveloperSigningCredential()
                 ;
 
         }
